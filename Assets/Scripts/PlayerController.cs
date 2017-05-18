7a08942881502629caadc6b4b51dfe11f8d34a70
Brian Boyko <brian.boyko@gmail.com>
2017-05-18 18:58:31 -0500

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; 
+
+	private Rigidbody rb;
+
+	void Start (){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// FixedUpdate is performed just before rendering any physics
+	// calculations
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
+
+		rb.AddForce (movement * speed);
+	}
+	// apply that input to the Player as movement.
+}
